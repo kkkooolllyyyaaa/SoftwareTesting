5d4f2c638747e385d9f11a022dc4f95ac76411d5
flyfalldie <kolya_cypandin@mail.ru>
2023-03-22 11:51:18 +0300

    csv added

--- Lab2/src/test/kotlin/util/CsvWriter.kt
+++ Lab2/src/test/kotlin/util/CsvWriter.kt
@@ -2,41 +2,13 @@ package util
 
 import functions.base.Sinus
 import org.junit.jupiter.api.Test
+import testing.TestValues
 import java.io.FileOutputStream
 import java.io.OutputStream
 
 class CsvWriter {
     private val update = true
     private val baseDir = "src/test/resources"
-
-    private val testingValues = listOf(
-        -100.0,
-        100.0,
-        -50.0,
-        50.0,
-        -25.0,
-        25.0,
-        -10.0,
-        10.0,
-        -6.28318,
-        6.28318,
-        -5.0,
-        5.0,
-        -3.14159,
-        3.14159,
-        -1.57079,
-        1.57079,
-        -1.04719,
-        1.04719,
-        -0.78539,
-        0.78539,
-        -0.52359,
-        0.52359,
-        -0.25,
-        0.25,
-        -0.1,
-        0.1,
-    )
 
     @Test
     fun writeAll() {
@@ -46,7 +18,7 @@ class CsvWriter {
 
         val sin = Sinus()
         var results = mutableListOf<Pair<Double, Double>>()
-        testingValues.forEach {
+        TestValues.X.forEach {
             results.add(it to sin.valueDecomposed(it.toBigDecimal()).toDouble())
         }
         FileOutputStream("$baseDir/sin.csv").writeCsv(results)
@@ -54,7 +26,7 @@ class CsvWriter {
         results = mutableListOf()
     }
 
-    fun OutputStream.writeCsv(xAndY: List<Pair<Double, Double>>) {
+    private fun OutputStream.writeCsv(xAndY: List<Pair<Double, Double>>) {
         val writer = bufferedWriter()
         xAndY.forEach {
             writer.write("${it.first}, ${it.second}")
