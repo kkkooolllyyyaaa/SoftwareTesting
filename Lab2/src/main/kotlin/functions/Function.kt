16d3b44761c54a46a36faaf226b02c764da1a2fe
flyfalldie <kolya_cypandin@mail.ru>
2023-03-22 12:06:32 +0300

    Function ready

--- Lab2/src/main/kotlin/functions/Function.kt
+++ Lab2/src/main/kotlin/functions/Function.kt
@@ -6,18 +6,21 @@ import functions.nonbase.Logarithm
 import functions.nonbase.Secant
 import java.math.BigDecimal
 
-class Function(private val naturalLogarithm: NaturalLogarithm, private val cosine: Cosine, private val secant: Secant) :
-    MathFunction {
-    private var log_2 = Logarithm(naturalLogarithm, BigDecimal(2))
-    private var log_3 = Logarithm(naturalLogarithm, BigDecimal(3))
-    private var log_5 = Logarithm(naturalLogarithm, BigDecimal(5))
-    private var log_10 = Logarithm(naturalLogarithm, BigDecimal(10))
+class Function(
+    private val naturalLogarithm: NaturalLogarithm,
+    private val cosine: Cosine,
+    private val secant: Secant,
+) : MathFunction {
+    private var log2 = Logarithm(naturalLogarithm, BigDecimal(2))
+    private var log3 = Logarithm(naturalLogarithm, BigDecimal(3))
+    private var log5 = Logarithm(naturalLogarithm, BigDecimal(5))
+    private var log10 = Logarithm(naturalLogarithm, BigDecimal(10))
 
     override fun value(x: BigDecimal): BigDecimal {
         return if (x > BigDecimal(0)) {
-            ((((log_10.value(x) / log_3.value(BigDecimal(3))) * (log_5.value(x) / log_2.value(x))) / log_10.value(
+            ((((log10.value(x) / log3.value(BigDecimal(3))) * (log5.value(x) / log2.value(x))) / log10.value(
                 x
-            )) * ((log_5.value(x) * naturalLogarithm.value(x)) * (naturalLogarithm.value(x) * log_3.value(x)))).pow(3)
+            )) * ((log5.value(x) * naturalLogarithm.value(x)) * (naturalLogarithm.value(x) * log3.value(x)))).pow(3)
         } else {
             cosine.value(x) + secant.value(x)
         }
@@ -25,15 +28,15 @@ class Function(private val naturalLogarithm: NaturalLogarithm, private val cosin
 
     override fun valueDecomposed(x: BigDecimal, eps: BigDecimal): BigDecimal {
         return if (x > BigDecimal(0)) {
-            ((((log_10.valueDecomposed(x, eps) / log_3.valueDecomposed(BigDecimal(3), eps)) * (log_5.valueDecomposed(
+            ((((log10.valueDecomposed(x, eps) / log3.valueDecomposed(BigDecimal(3), eps)) * (log5.valueDecomposed(
                 x,
                 eps
-            ) / log_2.valueDecomposed(x, eps))) / log_10.valueDecomposed(
+            ) / log2.valueDecomposed(x, eps))) / log10.valueDecomposed(
                 x, eps
-            )) * ((log_5.valueDecomposed(x, eps) * naturalLogarithm.valueDecomposed(
+            )) * ((log5.valueDecomposed(x, eps) * naturalLogarithm.valueDecomposed(
                 x,
                 eps
-            )) * (naturalLogarithm.valueDecomposed(x, eps) * log_3.valueDecomposed(x, eps)))).pow(3)
+            )) * (naturalLogarithm.valueDecomposed(x, eps) * log3.valueDecomposed(x, eps)))).pow(3)
         } else {
             cosine.valueDecomposed(x, eps) + secant.valueDecomposed(x, eps)
         }
@@ -41,19 +44,17 @@ class Function(private val naturalLogarithm: NaturalLogarithm, private val cosin
 
     override fun valueDecomposed(x: BigDecimal, n: Int): BigDecimal {
         return if (x > BigDecimal(0)) {
-            ((((log_10.valueDecomposed(x, n) / log_3.valueDecomposed(BigDecimal(3), n)) * (log_5.valueDecomposed(
+            ((((log10.valueDecomposed(x, n) / log3.valueDecomposed(BigDecimal(3), n)) * (log5.valueDecomposed(
                 x,
                 n
-            ) / log_2.valueDecomposed(x, n))) / log_10.valueDecomposed(
+            ) / log2.valueDecomposed(x, n))) / log10.valueDecomposed(
                 x, n
-            )) * ((log_5.valueDecomposed(x, n) * naturalLogarithm.valueDecomposed(
+            )) * ((log5.valueDecomposed(x, n) * naturalLogarithm.valueDecomposed(
                 x,
                 n
-            )) * (naturalLogarithm.valueDecomposed(x, n) * log_3.valueDecomposed(x, n)))).pow(3)
+            )) * (naturalLogarithm.valueDecomposed(x, n) * log3.valueDecomposed(x, n)))).pow(3)
         } else {
             cosine.valueDecomposed(x, n) + secant.valueDecomposed(x, n)
         }
     }
-
-
 }
