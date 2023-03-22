15ffe357fe8b07d541e9fda499911a2e2b17229c
nmakarov <311735@niuitmo.ru>
2023-03-22 12:06:46 +0300

    Logarithm tests done

--- Lab2/src/test/kotlin/functions/nonbase/LogarithmTest.kt
+++ Lab2/src/test/kotlin/functions/nonbase/LogarithmTest.kt
@@ -2,7 +2,6 @@ package functions.nonbase
 
 import exception.InputValidationException
 import functions.base.NaturalLogarithm
-import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvFileSource
 import util.assertAlmostEqual
@@ -14,9 +13,48 @@ class LogarithmTest {
     private val naturalLogarithm = NaturalLogarithm()
 
     @ParameterizedTest
-    @CsvFileSource(resources = ["/Logarithm.csv"])
+    @CsvFileSource(resources = ["/log2.csv"])
     fun `logarithm_2_test`(value: BigDecimal, expected: String) {
         val logarithm = Logarithm(naturalLogarithm, BigDecimal(2))
+        if (isBigDecimal(expected))
+            assertAlmostEqual(BigDecimal(expected), logarithm.value(value))
+        else
+            assertFailsWith<InputValidationException>(
+                message = "Логарифм от отрицательного числа",
+                block = { logarithm.value(value) }
+            )
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = ["/log3.csv"])
+    fun `logarithm_3_test`(value: BigDecimal, expected: String) {
+        val logarithm = Logarithm(naturalLogarithm, BigDecimal(3))
+        if (isBigDecimal(expected))
+            assertAlmostEqual(BigDecimal(expected), logarithm.value(value))
+        else
+            assertFailsWith<InputValidationException>(
+                message = "Логарифм от отрицательного числа",
+                block = { logarithm.value(value) }
+            )
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = ["/log5.csv"])
+    fun `logarithm_5_test`(value: BigDecimal, expected: String) {
+        val logarithm = Logarithm(naturalLogarithm, BigDecimal(5))
+        if (isBigDecimal(expected))
+            assertAlmostEqual(BigDecimal(expected), logarithm.value(value))
+        else
+            assertFailsWith<InputValidationException>(
+                message = "Логарифм от отрицательного числа",
+                block = { logarithm.value(value) }
+            )
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = ["/log10.csv"])
+    fun `logarithm_10_test`(value: BigDecimal, expected: String) {
+        val logarithm = Logarithm(naturalLogarithm, BigDecimal(10))
         if (isBigDecimal(expected))
             assertAlmostEqual(BigDecimal(expected), logarithm.value(value))
         else
